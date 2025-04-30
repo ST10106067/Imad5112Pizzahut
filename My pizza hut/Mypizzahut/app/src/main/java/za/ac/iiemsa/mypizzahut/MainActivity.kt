8d647d791187957395c024df0dff37f5d75c907e
ST10106067 <ST10106067@imconnect.edu.za>
2025-04-30 15:48:36 +0200

    First Commit

---
+++ My pizza hut/Mypizzahut/app/src/main/java/za/ac/iiemsa/mypizzahut/MainActivity.kt
@@ -0,0 +1,71 @@
+package za.ac.iiemsa.mypizzahut
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+
+        val radioGroup = findViewById<RadioGroup>(R.id.RadioGroup)
+        val chkSweetChillie = findViewById<CheckBox>(R.id.chkSweetChillie)
+        val chkCheese = findViewById<CheckBox>(R.id.chkCheese)
+        val btnCalculate = findViewById<Button>(R.id.btnCalculate)
+        val btnReset = findViewById<Button>(R.id.btnReset)
+        val txtResults = findViewById<TextView>(R.id.txtResults)
+        val rbSmall = findViewById<RadioButton>(R.id.rbSmall)
+        val rbMedium = findViewById<RadioButton>(R.id.rbMedium)
+        val rbLarge = findViewById<RadioButton>(R.id.rbLarge)
+
+
+        btnCalculate.setOnClickListener{
+            var total = 0.0
+                    if (rbSmall.isChecked) {
+                        total += 55.99// total = total + 55.99
+                    } else if (rbMedium.isChecked) {
+                        total += 89.99
+                    } else if (rbLarge.isChecked) {
+                        total += 119.99
+                    }
+
+
+            if (chkCheese.isChecked) {
+                total += 5.50
+            }
+            if (chkSweetChillie. isChecked) {
+                total += 3.50
+            }
+
+            txtResults.text = String.format("Total Amount Due: R%.2f" , total)
+            }
+        btnReset.setOnClickListener(){
+            txtResults.text = ""
+            //radioGroupSize.ClearCheck()
+            rbSmall.isChecked = false
+            rbMedium.isChecked = false
+            rbLarge.isChecked= false
+            chkCheese.isChecked =false
+            chkCheese.isChecked =false
+
+
+        }
+        
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
